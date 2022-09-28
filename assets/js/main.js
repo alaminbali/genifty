3f766ffb2f64925e376fc1df4fd25fbdc0dcd595
Mahabub Alam <msojib.cs@gmail.com>
2022-09-28 16:53:54 +0600

    issue fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,7 @@
           imJs.searchClick();
           imJs.uploadimg();
           imJs.vedioActivation();
+          imJs.cursorAnimate();
       },
       swiperActivation: function (){
         $(document).ready(function() {
@@ -92,10 +93,14 @@
         $(document).ready(function() {
           var swiper = new Swiper(".LiveBidding", {
             slidesPerView: 5,
-            spaceBetween: 10,
+            spaceBetween: 0,
             slidesPerGroup: 1,
             loop: true,
             loopFillGroupWithBlank: true,
+            pagination: {
+              el: ".swiper-pagination",
+              clickable: true,
+            },
             navigation: {
               nextEl: ".swiper-button-next",
               prevEl: ".swiper-button-prev",
@@ -339,6 +344,24 @@
             $('.video-overlay.open').removeClass('open').find('iframe').remove();
         };
       },
+      cursorAnimate: function () {
+        var myCursor = $('.mouse-cursor');
+        if (myCursor.length) {
+            if ($('body')) {
+                const e = document.querySelector('.cursor-inner'),
+                    t = document.querySelector('.cursor-outer');
+                let n, i = 0,
+                    o = !1;
+                window.onmousemove = function (s) {
+                    o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
+                }, $('body').on("mouseenter", "a, .cursor-pointer", function () {
+                    e.classList.add('cursor-hover'), t.classList.add('cursor-hover')
+                }), $('body').on("mouseleave", "a, .cursor-pointer", function () {
+                    $(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove('cursor-hover'), t.classList.remove('cursor-hover'))
+                }), e.style.visibility = "visible", t.style.visibility = "visible"
+            }
+        }
+    },
   }
 
   imJs.m();
